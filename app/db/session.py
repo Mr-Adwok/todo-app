d23b9cdc33cfd4556c9e8181a7e1d01c239a2e02
Mr-Adwok <adwokbrave@gmail.com>
2026-02-25 09:08:56 +0200

    fix: connection problems

--- app/db/session.py
+++ app/db/session.py
@@ -1,5 +1,5 @@
 from sqlmodel import SQLModel
-from app.settings import config
+from settings import config
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import AsyncEngine,create_async_engine
 from sqlmodel.ext.asyncio.session import AsyncSession
@@ -12,8 +12,10 @@ async_engine = create_async_engine(url = config.DATABASE_URL, echo=True)
 
 async def init_db():
     async with async_engine.connect() as conn:
+        from models.todo import Todo
         print("Database connected")
         await conn.run_sync(SQLModel.metadata.create_all)
+        print(SQLModel.metadata.tables)
 
 
 
