33b6ad53b14055167ed720e7028ddebeb55de029
Mr-Adwok <adwokbrave@gmail.com>
2026-02-25 09:10:50 +0200

    Add: features to the model

--- app/models/todo.py
+++ app/models/todo.py
@@ -1,14 +1,22 @@
 import uuid
-from sqlmodel import Field, SQLModel
+from datetime import datetime
+from sqlmodel import Field, SQLModel,Column
 import sqlalchemy.dialects.postgresql as pg
 
 # an example mapping using the base
 class Todo(SQLModel,table = True):
+    __tablename__ = "todo1"
 
     uid:uuid.UUID = Field(
         sa_column=Column(pg.UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, nullable=False)
     )
     title:str
     completed:bool
-    created_at:datetime = Field(default_factory = datetime.utcnow)
+    created_at:datetime = Field(default_factory=datetime.now)
+
+    def __repr__(self):
+        return f"<Book {self.title}>"
+
+    class Config:
+        orm_mode = True
 
