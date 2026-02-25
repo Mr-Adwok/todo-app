32bf65a55d39448b2093d7990860f19f01db1852
Mr-Adwok <adwokbrave@gmail.com>
2026-02-25 09:40:48 +0200

    Add: first service create_todo for the route

--- app/services/all_services.py
+++ app/services/all_services.py
@@ -2,6 +2,7 @@ from .models.todo import Todo
 from datetime import datetime
 from db.session import session
 from sqlmodel import select,desc
+from schemas.schema import CreateTodo
 # from db.session import get_session
 
 
@@ -9,6 +10,16 @@ from sqlmodel import select,desc
 
 class Service_Act():
 
-    async def todoPost(self,session:AsyncSession):
+    async def create_todo(self,session:AsyncSession):
+
+        todo = CreateTodo().model_dump()
+        new_todo = Todo(**todo)
+
+        session.add(new_todo)
+        await session.commit()
+
+        return new_todo
+
+
 
 
