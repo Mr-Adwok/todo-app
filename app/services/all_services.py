bb4cf8771a436966a7feae3317f2d10a48dc7a13
Mr-Adwok <adwokbrave@gmail.com>
2026-03-02 08:58:29 +0200

    Add: some pydantic logic to help with creating the class Todo

--- app/services/all_services.py
+++ app/services/all_services.py
@@ -1,23 +1,25 @@
-from .models.todo import Todo
-from datetime import datetime
-from db.session import session
+from app.models.todo import Todo
+
 from sqlmodel import select,desc
-from schemas.schema import CreateTodo
+from app.schemas.schema import CreateTodo
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+
 # from db.session import get_session
 
 
 
 
-class Service_Act():
+class TodoService():
 
-    async def create_todo(self,session:AsyncSession):
+    async def create_todo(self,todo:CreateTodo,session:AsyncSession):
 
-        todo = CreateTodo().model_dump()
-        new_todo = Todo(**todo)
+        todo_dict = todo.model_dump()
+        new_todo = Todo(**todo_dict)
 
         session.add(new_todo)
         await session.commit()
-
+        await session.refresh(new_todo)
         return new_todo
 
 
