ee8284f800b58349b3e58557871f7d0f71bc71b3
Mr-Adwok <adwokbrave@gmail.com>
2026-03-30 07:07:29 +0200

    Add: get_all_todo service

--- app/services/all_services.py
+++ app/services/all_services.py
@@ -12,6 +12,7 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 
 class TodoService():
 
+    # create todo
     async def create_todo(self,todo:CreateTodo,session:AsyncSession):
 
         todo_dict = todo.model_dump()
@@ -23,5 +24,23 @@ class TodoService():
         return new_todo
 
 
+    # get all todo
+
+    async def get_todos(self,session:AsyncSession):
+        statement = select(Todo)
+        get_all_todo = await session.exec(statement)
+        results =  get_all_todo.all()
+        print("Length:", len(results))
+        print(results[0])
+        return [
+    {
+        "uid": str(todo.uid),
+        "title": todo.title,
+        "description": todo.description,
+        "completed": todo.completed,
+        "created_at": todo.created_at.isoformat()
+    }
+    for todo in results  # ✅ use 'results', not 'todos'
+]
 
 
